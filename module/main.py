7ce07ccd8ceba83c4c951152bdc90196fc43c1b5
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2022-12-15 17:20:21 +0900

    Upload: separate modules

--- module/main.py
+++ module/main.py
@@ -1,17 +1,15 @@
-# main
-
-# module import
-## 라이브러리 설치 및 불러오기
-from preparation import P_install 
-
-## 팀 컬러 크롤링 코드
-import team_color_crawling
-from team_color_crawling import TCC_main
+# TCC(Team color crawling) main
+## module import
+import data_io
+import TCL_crawling
+import TCD_crawling
 
 
-# 사용할 라이브러리 설치 및 불러오기
-P_install()
+## run
+# teamcolor_types = TCL_crawling.Team_color_list()
+### teamcolor list 데이터 로드 : 새로 팀컬러 목록을 크롤링 하지 않고 기존의 저장된 목록을 불러올 때 사용!!
+teamcolor_type = data_io.load_TCL_data()
 
-# 팀 컬러 크롤링
-df_teamcolor_club,df_teamcolor_nation,df_teamcolor_reinforce,df_teamcolor_relation,df_teamcolor_special = TCC_main()
-
+df_teamcolor_club,df_teamcolor_nation,df_teamcolor_reinforce,df_teamcolor_relation,df_teamcolor_special = TCD_crawling.team_color_detail(teamcolor_type)
+### teamcolor detail 데이터 로드
+# df_teamcolor_club,df_teamcolor_nation,df_teamcolor_reinforce,df_teamcolor_relation,df_teamcolor_special = data_io.load_TCL_data()
